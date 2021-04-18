28339d1be4da07e7c4a5b0a69c663914381017c1
jwalk057 <77030819+jwalk057@users.noreply.github.com>
2021-04-18 16:00:41 -0700

    Update jwalk057_lab4_part1.c

--- turnin/jwalk057_lab4_part1.c
+++ turnin/jwalk057_lab4_part1.c
@@ -17,19 +17,19 @@ enum SM {SM_Start, SM_WaitB0, SM_B1, SM_WaitB1} sm1;
         void Tick(){
                 switch(sm1){
                         case SM_Start:
-                                if(PINA & 0x01){sm1 = SM_Start;}
+                                if(PINA == 0x01){sm1 = SM_Start;}
                                 else{sm1 = SM_WaitB0;}
                                 tmpB = 0x01;
                                 PORTB = tmpB;
                                 break;
 
                         case SM_WaitB0:
-                                if(PINA & 0x01){sm1 = SM_B1;}
+                                if(PINA == 0x01){sm1 = SM_B1;}
                                 else{sm1 = SM_WaitB0;}
                                 break;
 
                         case SM_B1:
-                                if(PINA & 0x01){sm1 = SM_B1;}
+                                if(PINA == 0x01){sm1 = SM_B1;}
                                 else{sm1 = SM_WaitB1;}
                                 tmpB = 0x02;
                                 PORTB = tmpB;
